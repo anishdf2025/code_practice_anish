eb25eb24ea4671609014c3670254f584a7a927a0
anish kumar maurya <anishdf2025@gmail.com>
2025-12-18 15:41:07 +0530

    new code

---
+++ euclidian.py
@@ -0,0 +1,9 @@
+import math
+x1=int(input("enter number x1"))
+y1=int(input("enter number y1"))
+x2=int(input("enter number x2"))
+y2=int(input("enter number y2"))
+
+distance=math.sqrt((x2-x1)**2+(y2-y1)**2)
+print(distance)
+
